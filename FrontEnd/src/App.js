9334f6fe0d45c5dbe1427bf0ac97a075879eea7b
Dario Ferreira França <dariofft@gmail.com>
2020-03-28 15:45:27 -0300

    Adicionado Projeto FrontEnd.

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './pages/Login'
+
+export default function App() {
+  return (
+    <div>
+      <Login title='Teste dario' />
+      <h1>TESTE COR</h1>
+    </div>
+  );
+}
+
